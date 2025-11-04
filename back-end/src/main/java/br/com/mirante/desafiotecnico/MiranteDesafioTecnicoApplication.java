76cd6d563a0c5f482a4783fa833186962814eaa8
Iago Baldani <iagobaldanialmeida@hotmail.com>
2025-11-03 23:29:32 -0300

    feat(api): Base da aplicacao

---
+++ back-end/src/main/java/br/com/mirante/desafiotecnico/MiranteDesafioTecnicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.mirante.desafiotecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiranteDesafioTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiranteDesafioTecnicoApplication.class, args);
+	}
+
+}
